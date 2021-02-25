dbce3c852f1b7f213dfbfadf864e4eac3b6f206f
German <coolmatoff@gmail.com>
2021-02-25 15:16:43 +0600

    added admin

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Article
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+    search_fields = ['title', 'content']
+    fields = ['id', 'title', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at', 'id']
+
+
+admin.site.register(Article, ArticleAdmin)
--- hello/webapp/models.py
+++ hello/webapp/models.py
@@ -3,4 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    pass
+    title = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Задача')
+    status = models.CharField(max_length=200, null=False, blank=False, default='Unknown', verbose_name='Статус')
+    time = models.CharField(max_length=200, null=True, blank=True, default='New')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    class Meta:
+        db_table = 'articles'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Статус'
+
+    def __str__(self):
+
+        return f'{self.title} {self.status} {self.time}'
