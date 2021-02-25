0547c19e3651aef131fbb0a52821194d9543433f
German <coolmatoff@gmail.com>
2021-02-25 16:02:57 +0600

    Some alpha view of first tasks page

--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from webapp.models import Article
 
 # Create your views here.
+
+def index_view(request):
+    articles = Article.objects.all()
+    return render(request, 'index.html', context={'articles': articles})
